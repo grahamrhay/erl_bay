d698dbf61e71d10faae041471ece828fe9b5eda0
Graham Hay <grahamrhay@gmail.com>
2015-01-26 19:05:54 +0000

    Basic bidding

--- src/eb_auction.erl
+++ src/eb_auction.erl
@@ -2,7 +2,7 @@
 -behaviour(gen_server).
 
 %% API.
--export([start_link/0]).
+-export([start_link/1]).
 
 %% gen_server.
 -export([init/1]).
@@ -12,19 +12,42 @@
 -export([terminate/2]).
 -export([code_change/3]).
 
--record(state, {
-         }).
+-record(state, {starting_price, bids=[]}).
+-record(bid, {bidder, amount, time}).
 
 %% API.
 
--spec start_link() -> {ok, pid()}.
-start_link() ->
-    gen_server:start_link(?MODULE, [], []).
+-type money() :: {integer(), integer()}.
+-spec start_link([money()]) -> {ok, pid()}.
+start_link(StartingPrice) ->
+    gen_server:start_link(?MODULE, [StartingPrice], []).
 
 %% gen_server.
 
-init([]) ->
-    {ok, #state{}}.
+init([StartingPrice]) ->
+    {ok, #state{starting_price = StartingPrice}}.
+
+handle_call({bid, _UserId, Bid}, _From, State) when Bid < State#state.starting_price ->
+    {reply, bid_too_low, State};
+
+handle_call({bid, UserId, BidAmount}, _From, State) when State#state.bids =:= [] ->
+    Bid = #bid{bidder = UserId, amount = BidAmount, time = calendar:universal_time()},
+    NewState = State#state{bids = [Bid | State#state.bids]},
+    {reply, bid_accepted, NewState};
+
+handle_call({bid, UserId, BidAmount}, _From, State) ->
+    CurrentHighBid = hd(State#state.bids),
+    case BidAmount > CurrentHighBid#bid.amount of
+        true ->
+            Bid = #bid{bidder = UserId, amount = BidAmount, time = calendar:universal_time()},
+            NewState = State#state{bids = [Bid | State#state.bids]},
+            {reply, bid_accepted, NewState};
+        false ->
+            {reply, bid_too_low, State}
+    end;
+
+handle_call(list_bids, _From, State) ->
+    {reply, State#state.bids, State};
 
 handle_call(Request, _From, State) ->
     lager:error("Unexpected call: ~p", [Request]),
--- test/eb_auction_tests.erl
+++ test/eb_auction_tests.erl
@@ -3,17 +3,50 @@
 -include_lib("eunit/include/eunit.hrl").
 
 gen_server_test_() ->
-    {foreach, fun setup/0, fun cleanup/1, [fun(Pid) -> [
-        fun() -> server_is_alive(Pid) end
-    ] end]}.
+    {foreach, fun setup/0, fun cleanup/1, [
+        fun list_of_bids_is_empty_at_start/1,
+        fun reject_bid_lower_than_starting_price/1,
+        fun first_bid_is_accepted/1,
+        fun bid_rejected_if_lower_than_previous_bid/1,
+        fun bid_accepted_if_higher_than_current_bid/1
+    ]}.
 
 setup() ->
     process_flag(trap_exit, true),
-    {ok, Pid} = eb_auction:start_link(),
+    StartingPrice = {0, 99},
+    {ok, Pid} = eb_auction:start_link(StartingPrice),
     Pid.
 
-server_is_alive(Pid) ->
-    ?assertEqual(true, is_process_alive(Pid)).
+list_of_bids_is_empty_at_start(Pid) ->
+    fun() ->
+        ?assertEqual([], gen_server:call(Pid, list_bids))
+    end.
+
+reject_bid_lower_than_starting_price(Pid) ->
+    fun() ->
+        ?assertEqual(bid_too_low, gen_server:call(Pid, {bid, 1, {0, 70}}))
+    end.
+
+first_bid_is_accepted(Pid) ->
+    fun() ->
+        bid_accepted = gen_server:call(Pid, {bid, 1, {1, 99}}),
+        [Bid] = gen_server:call(Pid, list_bids),
+        ?assertMatch({bid, 1, {1, 99}, _Time}, Bid)
+    end.
+
+bid_rejected_if_lower_than_previous_bid(Pid) ->
+    fun() ->
+        bid_accepted = gen_server:call(Pid, {bid, 1, {1, 99}}),
+        ?assertEqual(bid_too_low, gen_server:call(Pid, {bid, 2, {0, 99}}))
+    end.
+
+bid_accepted_if_higher_than_current_bid(Pid) ->
+    fun() ->
+        bid_accepted = gen_server:call(Pid, {bid, 1, {1, 99}}),
+        bid_accepted = gen_server:call(Pid, {bid, 2, {2, 99}}),
+        HighBid = hd(gen_server:call(Pid, list_bids)),
+        ?assertMatch({bid, 2, {2, 99}, _Time}, HighBid)
+    end.
 
 cleanup(Pid) ->
     exit(Pid, kill), %% brutal kill!
